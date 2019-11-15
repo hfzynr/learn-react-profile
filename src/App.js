392d7c0b7a9fa86bb4744876debc28aa91e39de7
Hafiz Yanuar Ibrahim <hafizyanuar95@gmail.com>
2019-11-15 15:38:21 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <img src={`${process.env.PUBLIC_URL}/assets/img/1.jpeg`} className="App-logo rounded-circle" alt="logo" />
+
+      <div className="jumbotron" id="jumbotron">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nama saya Hafiz Yanuar Ibrahim  <br/>
+          saya lulus dari SMK 1 Jakarta pada tahun 2017.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        For more detail :
+        <a className="App-link" href="https://github.com/hfzynr">
+          Click Me !
         </a>
+      </div>
+
       </header>
     </div>
   );
